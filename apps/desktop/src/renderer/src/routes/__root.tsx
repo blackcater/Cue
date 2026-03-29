c4a5424d6d71ece9262cc2767777e3daab8ea71d
Elon Tang <i@blackcater.dev>
2026-03-29 16:51:44 +0800

    feat(desktop): integrate AppShell into root route

--- apps/desktop/src/renderer/src/routes/__root.tsx
+++ apps/desktop/src/renderer/src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from '@tanstack/react-router'
+import { AppShell } from '../components/app-shell'
 
 export function RootComponent(): React.JSX.Element {
 	return (
-		<div className="flex h-screen bg-transparent">
+		<AppShell>
 			<main className="flex flex-1 flex-col">
 				<Outlet />
 			</main>
-		</div>
+		</AppShell>
 	)
 }
